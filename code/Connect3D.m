961fa19cc0376a28bb10225673cbe9b6b150a3e6
huangjiancong1 <huangjiancong1@github.com>
2018-04-18 18:22:56 +0800

    Inverse_Kinematics

---
+++ code/Connect3D.m
@@ -0,0 +1,7 @@
+% Author: huangjiancong
+% Function: draw the arm
+% Date: 2018/04/18
+function Connect3D(p1,p2,option,pt)
+
+h = plot3([p1(1) p2(1)],[p1(2) p2(2)],[p1(3) p2(3)],option);
+set(h,'LineWidth',pt)
